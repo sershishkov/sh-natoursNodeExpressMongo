21762dc8737e76978e0179d9fdb09b481e4963db
sershishkov <ser.shishkov@gmail.com>
2019-11-01 18:51:06 +0200

    6-7

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res
@@ -25,6 +26,24 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 const PORT = 3000;
