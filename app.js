06fb00893185f5a24538644531ac21de016d1745
sershishkov <ser.shishkov@gmail.com>
2019-11-01 19:57:42 +0200

    6-10

--- app.js
+++ app.js
@@ -79,6 +79,19 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length - 1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id'
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
