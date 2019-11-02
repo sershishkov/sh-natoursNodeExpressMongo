59cda8ec1b75e73ad7f6e80926252c2fcec8924d
sershishkov <ser.shishkov@gmail.com>
2019-11-02 16:09:58 +0200

    6-14

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+//MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,7 +21,7 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-
+//ROUTE HANDLER
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -99,6 +103,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+//ROUTES
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -110,6 +115,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// START SERVER
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
