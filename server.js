8f3faeebb497e59e50c487d9a04c287d126c7924
sershishkov <ser.shishkov@gmail.com>
2019-11-03 10:21:04 +0200

    8-4

--- server.js
+++ server.js
@@ -18,8 +18,25 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
-const PORT = process.env.PORT || 3000;
+const tourScema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
 
+const Tour = mongoose.model('Tour', tourScema);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
