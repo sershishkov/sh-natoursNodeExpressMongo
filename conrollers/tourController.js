31c29f6f6dc4a09d9d568c803be5fb386f492cd4
sershishkov <ser.shishkov@gmail.com>
2019-11-03 16:13:11 +0200

    8-12

--- conrollers/tourController.js
+++ conrollers/tourController.js
@@ -50,7 +50,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'FAIL',
-      message: 'Invalid data sent!'
+      message: err
     });
   }
 };
