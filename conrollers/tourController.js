a030369fe57cc0f9504385cfc73a1ea7cffa80b7
sershishkov <ser.shishkov@gmail.com>
2019-11-03 17:52:08 +0200

    8-14

--- conrollers/tourController.js
+++ conrollers/tourController.js
@@ -2,7 +2,21 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    const queryObj = { ...req.query };
+    const exludedFields = ['page', 'sort', 'limit', 'fields'];
+    exludedFields.forEach(el => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // const query =  Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    //EXECUTE QUERY
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
