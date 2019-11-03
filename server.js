715453b9cae9062bdc4774c627b20a9a4d0d655e
sershishkov <ser.shishkov@gmail.com>
2019-11-03 10:36:17 +0200

    8-5

--- server.js
+++ server.js
@@ -36,6 +36,21 @@ const tourScema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourScema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+
+  price: 497
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR :', err);
+  });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
