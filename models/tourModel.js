9ba6898ea2a5d25ec2d908c9863106060b982ad6
sershishkov <ser.shishkov@gmail.com>
2019-11-03 19:08:37 +0200

    8-17

--- models/tourModel.js
+++ models/tourModel.js
@@ -48,7 +48,8 @@ const tourScema = new mongoose.Schema({
   images: [String],
   createdAt: {
     type: Date,
-    default: Date.now()
+    default: Date.now(),
+    select: false
   },
   startDates: [Date]
 });
