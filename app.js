459a792f4e6d4ef6bc4346f128742ed95319556e
sershishkov <ser.shishkov@gmail.com>
2019-11-07 19:55:13 +0200

    9-5

--- app.js
+++ app.js
@@ -23,9 +23,24 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.json({
-    status: 'Fail',
-    message: `Can't find ${req.originalUrl} on this server`
+  // res.json({
+  //   status: 'Fail',
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'Fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
