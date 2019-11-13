f8b6fe3ab53b2cbab1e32d291bbee887d1100abe
sershishkov <ser.shishkov@gmail.com>
2019-11-13 20:39:03 +0200

    10-21

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./conrollers/errorController');
@@ -9,11 +10,14 @@ const userRouter = require('./routes/usersRoutes');
 
 const app = express();
 // GLOBAL MIDDLEWARES
+//Set security HTTP headers
+app.use(helmet());
 
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+//Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -21,12 +25,14 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
-
-app.use(express.json());
+//BODY parser - reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log(req.headers)
   next();
 });
 
