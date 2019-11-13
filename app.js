8e5f45904a672b2214c6fac7cd59181281dc21e4
sershishkov <ser.shishkov@gmail.com>
2019-11-13 20:25:54 +0200

    10-20

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./conrollers/errorController');
@@ -7,13 +8,21 @@ const tourRouter = require('./routes/toursRoutes');
 const userRouter = require('./routes/usersRoutes');
 
 const app = express();
-//MIDDLEWARES
-// console.log(process.env.NODE_ENV);
+// GLOBAL MIDDLEWARES
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, Please try again in an hour'
+});
+
+app.use('/api', limiter);
+
+app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
