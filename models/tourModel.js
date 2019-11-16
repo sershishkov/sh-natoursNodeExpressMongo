fdb5115ae424a76c2a82ede2a608cc6c568cc66f
sershishkov <ser.shishkov@gmail.com>
2019-11-16 15:29:19 +0200

    11-5

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 
 const tourScema = new mongoose.Schema(
   {
@@ -100,7 +101,8 @@ const tourScema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -114,6 +116,12 @@ tourScema.virtual('durationWeeks').get(function() {
 //DOCUMENT MIDDLEWARE: runs before .save() and .create() only
 tourScema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourScema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
