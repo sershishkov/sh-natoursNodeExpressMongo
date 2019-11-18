f775ebb4d43811e48141434d24090fb8408639f0
sershishkov <ser.shishkov@gmail.com>
2019-11-18 20:47:33 +0200

    11-23

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -64,17 +64,33 @@ reviewSchema.statics.calcAverageRatings = async function(tourId) {
       }
     }
   ]);
-  console.log(stats);
-
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsAverage: stats[0].avgRating,
-    ratingsQuantity: stats[0].nRating
-  });
+  // console.log(stats);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: stats[0].avgRating,
+      ratingsQuantity: stats[0].nRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: 4.5,
+      ratingsQuantity: 0
+    });
+  }
 };
 
 reviewSchema.post('save', function() {
   //this points to current review
   this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  this.r = await this.findOne();
+  // console.log(this.r);
+  next();
+});
+reviewSchema.post(/^findOneAnd/, async function() {
+  // await this.findOne(); does NOT work here, query has already exicute
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 module.exports = mongoose.model('Review', reviewSchema);
