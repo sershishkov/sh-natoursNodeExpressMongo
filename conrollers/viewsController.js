e89a457966602ec1887b270a3dd3a4ee88a1492b
sershishkov <ser.shishkov@gmail.com>
2019-11-20 20:24:40 +0200

    12-9

--- conrollers/viewsController.js
+++ conrollers/viewsController.js
@@ -1,9 +1,18 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const Tour = require('../models/tourModel');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
+  //Get tour data
+  const tours = await Tour.find();
+
+  //Build template
+
+  //render that template using tour data
+
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours
   });
 });
 
