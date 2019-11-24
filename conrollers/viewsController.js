caf7c36767c5198c625f1bedff5ab11b8589be1e
sershishkov <ser.shishkov@gmail.com>
2019-11-24 20:04:12 +0200

    12-20

--- conrollers/viewsController.js
+++ conrollers/viewsController.js
@@ -22,6 +22,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
     path: 'reviews',
     fields: 'review rating user'
   });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
   //Build template
 
   // Render template using data
