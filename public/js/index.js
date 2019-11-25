08921e795a7e4dc8d62b8a8f771d3226086bef88
sershishkov <ser.shishkov@gmail.com>
2019-11-25 21:02:47 +0200

    12-24

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,15 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
-import { updateData } from './updateSettings';
+import { updateSettings } from './updateSettings';
+import { async } from 'q';
 
 //Dom Element
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logiOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 //Values
 
@@ -31,11 +33,30 @@ if (loginForm) {
 if (logiOutBtn) logiOutBtn.addEventListener('click', logout);
 
 if (userDataForm) {
-  userDataForm.addEventListener('submit', e => {
+  userDataForm.addEventListener('submit', async e => {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
+    await updateSettings({ name, email }, 'data');
+  });
+}
 
-    updateData(name, email);
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+    document.querySelector('.btn--save-password').textContent =
+      'Save password.';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -3,21 +3,22 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateData = async (name, email) => {
+//type is either 'password' or 'data'
+export const updateSettings = async (data, type) => {
   // console.log(email, name);
   try {
+    const url =
+      type === 'password'
+        ? 'http://localhost:3000/api/v1/users/updateMyPassword'
+        : 'http://localhost:3000/api/v1/users/updateMe';
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:3000/api/v1/users/updateMe',
-
-      data: {
-        name,
-        email
-      }
+      url,
+      data
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
