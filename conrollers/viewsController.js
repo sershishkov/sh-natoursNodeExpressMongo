6c21f5362a53ff2dacd1cd70750b16a3a542b23b
sershishkov <ser.shishkov@gmail.com>
2019-11-25 18:26:33 +0200

    12-21

--- conrollers/viewsController.js
+++ conrollers/viewsController.js
@@ -40,3 +40,9 @@ exports.getLoginForm = catchAsync(async (req, res, next) => {
     title: 'Log into your account'
   });
 });
+
+exports.getAccount = (req, res, next) => {
+  res.status(200).render('account', {
+    title: 'Your account'
+  });
+};
