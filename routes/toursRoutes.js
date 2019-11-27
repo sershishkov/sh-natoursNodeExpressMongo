89547f34adc1fe9720e3059bee9f51f1d9a25003
sershishkov <ser.shishkov@gmail.com>
2019-11-27 20:45:01 +0200

    13-7

--- routes/toursRoutes.js
+++ routes/toursRoutes.js
@@ -9,7 +9,9 @@ const {
   getTourStats,
   getMonthlyPlan,
   getToursWithin,
-  getDistances
+  getDistances,
+  uploadTourImages,
+  resizeTourImages
 } = require('./../conrollers/tourController');
 
 const { protect, restrictTo } = require('./../conrollers/authController');
@@ -39,7 +41,13 @@ router
 router
   .route('/:id')
   .get(getTour)
-  .patch(protect, restrictTo('admin', 'lead-guide'), updateTour)
+  .patch(
+    protect,
+    restrictTo('admin', 'lead-guide'),
+    uploadTourImages,
+    resizeTourImages,
+    updateTour
+  )
   .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
