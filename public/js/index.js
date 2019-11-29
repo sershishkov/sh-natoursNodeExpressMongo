506788e06e0eea290ac2248196f6c74fd2d0e28b
sershishkov <ser.shishkov@gmail.com>
2019-11-29 18:30:47 +0200

    13-15

--- public/js/index.js
+++ public/js/index.js
@@ -3,34 +3,31 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
-import { async } from 'q';
+import { bookTour } from './stripe';
 
-//Dom Element
+// DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
-const logiOutBtn = document.querySelector('.nav__el--logout');
+const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
-//Values
-
-//Delegation
+// DELEGATION
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
   displayMap(locations);
 }
 
-if (loginForm) {
+if (loginForm)
   loginForm.addEventListener('submit', e => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-
     login(email, password);
   });
-}
 
-if (logiOutBtn) logiOutBtn.addEventListener('click', logout);
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
 if (userDataForm)
   userDataForm.addEventListener('submit', e => {
@@ -44,22 +41,28 @@ if (userDataForm)
     updateSettings(form, 'data');
   });
 
-if (userPasswordForm) {
+if (userPasswordForm)
   userPasswordForm.addEventListener('submit', async e => {
     e.preventDefault();
     document.querySelector('.btn--save-password').textContent = 'Updating...';
+
     const passwordCurrent = document.getElementById('password-current').value;
     const password = document.getElementById('password').value;
     const passwordConfirm = document.getElementById('password-confirm').value;
-
     await updateSettings(
       { passwordCurrent, password, passwordConfirm },
       'password'
     );
-    document.querySelector('.btn--save-password').textContent =
-      'Save password.';
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
   });
-}
+
+if (bookBtn)
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
+  });
