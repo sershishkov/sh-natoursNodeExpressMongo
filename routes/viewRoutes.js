afde54a519a6006e0047445168e6ebaca686bf7f
sershishkov <ser.shishkov@gmail.com>
2019-11-29 19:07:01 +0200

    13-17

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,12 +2,19 @@ const express = require('express');
 
 const viewsController = require('../conrollers/viewsController');
 const authController = require('../conrollers/authController');
+const bookingController = require('../conrollers/bookingController');
 
 const router = express.Router();
 
 // router.use();
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview
+);
+
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
